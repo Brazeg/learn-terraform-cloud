b88652dffdcbf66439efbea3eeac79ee4a14eb00
Bahlawan <ebra.parkour@gmail.com>
2022-07-01 12:45:27 +0200

    uncomment instance_type and instance_name

--- variables.tf
+++ variables.tf
@@ -2,14 +2,14 @@ variable "region" {
   description = "AWS region"
   default     = "eu-central-1"
 }
-/*
-variable "instance_type" {
+
+  variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t2.nano"
 }
-*/
-/*variable "instance_name" {
+
+  variable "instance_name" {
   description = "EC2 instance name, WHICH IS BEING OVERRRIDEN BY THE VALUE OF THE TERRAFORM VARIABLE  instance_name"
   default     = "Provisioned by Ibrahim in Terraform! wohooooo"
 }
-*/
+
