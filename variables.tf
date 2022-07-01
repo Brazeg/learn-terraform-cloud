87261d16a66fd0a8c20c5228a07de4fcd8fcb855
Bahlawan <ebra.parkour@gmail.com>
2022-07-01 12:39:58 +0200

    Removed/commented instance_type to be able to use the value stored in terraform.auto.tfvars

--- variables.tf
+++ variables.tf
@@ -2,12 +2,12 @@ variable "region" {
   description = "AWS region"
   default     = "eu-central-1"
 }
-
+/*
 variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t2.nano"
 }
-
+*/
 /*variable "instance_name" {
   description = "EC2 instance name, WHICH IS BEING OVERRRIDEN BY THE VALUE OF THE TERRAFORM VARIABLE  instance_name"
   default     = "Provisioned by Ibrahim in Terraform! wohooooo"
